d5d0a96154ee86797d9225279dded9ab0f558264
Nathan Gardea <natecgardea@gmail.com>
2023-05-25 15:31:04 -0700

    Adding damage

---
+++ Assets/Scripts/DamageableEntity.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Can be inherited by the player, enemies, or any object that will take damage and then be destroyed
+public class DamageableEntity : MonoBehaviour
+{
+    public float HP;
+
+    public virtual void TakeDamage(float damage) {
+        HP -= damage;
+
+        if (HP <= 0) {
+            Die();
+        }
+    }
+
+    public virtual void Die() {
+        Destroy(gameObject);
+    }
+}
