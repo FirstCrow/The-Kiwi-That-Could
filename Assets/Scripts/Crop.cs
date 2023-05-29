07340100d0832d274faaaae39e9720ab000a7b12
Plutzz <benjiongco@gmail.com>
2023-05-29 12:30:09 -0700

    Added Crop Growing System

--- Assets/Scripts/Crop.cs
+++ Assets/Scripts/Crop.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,12 +8,94 @@ using UnityEngine;
 public class Crop : MonoBehaviour
 {
     [Header("Crop Varibles")]
-    public int cropStage;           //Used for the animator to display crop sprites
-    public float daysToGrow;        //How many days it will take for the crop to be harvestable
-    public Animator animator;       //Used to change crop sprite/animation depending on crop stage
-    public Crop(int cropStage, float daysToGrow)
+    
+    public float daysPerStage;           //How many days each crop stage will take
+    public float numCropStages;          //How many crop stages this crop has (the final crop stage is when it is harvestable)
+    private int cropStage;               //Used for the animator to display crop sprites
+    private float stageDaysElapsed;      //How many days have passed this crop stage
+    private bool harvestable;            //is the crop harvestable
+
+    [Header("Player Varibles")]
+    private GameObject player;
+    private float playerRange;
+
+    [Header("Color Varibles")]
+    public Color outOfRangeColor;
+    public Color inRangeColor;
+    private SpriteRenderer rend;
+    private Color originalColor;
+
+    [Header("Links")]
+    public Animator animator;      //Used to change crop sprite/animation depending on crop stage
+
+    private void Start()
     {
-        this.cropStage = cropStage;
-        this.daysToGrow = daysToGrow;
+
+        // Initializes crop and player varibles
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerRange = FindObjectOfType<PlayerController>().GetCropRange();
+        cropStage = 1;
+        stageDaysElapsed = 0;
+        harvestable = false;
+        DayNightScript.current.onNewDay += cropNewDay;
+
+        // Gets original color
+        rend = GetComponent<SpriteRenderer>();
+        originalColor = rend.color;
     }
+
+    // Called whenever it is a new day via event in DayNightScript
+    // Logic for how long each crop stage is
+    public void cropNewDay()
+    {
+        stageDaysElapsed++;
+        if(stageDaysElapsed >= daysPerStage)
+        {
+            updateCropState();
+        }
+    }
+
+    // Called when the crop stage needs to be incremented
+    public void updateCropState()
+    {
+        stageDaysElapsed = 0;
+        cropStage++;
+        animator.SetInteger("cropStage", cropStage);
+
+        // If the crop is fully grown it is harvestable
+        if (cropStage >= numCropStages)
+        {
+            harvestable = true;
+            Debug.Log("Plant ready to harvest!");
+        }
+    }
+
+    private void harvestCrop()
+    {
+
+    }
+
+    private void OnMouseOver()
+    {
+        float distanceFromPlant = Mathf.Abs((transform.position - player.transform.position).magnitude);
+        if (!PauseMenu.getGameIsPaused())
+        {
+            if (distanceFromPlant <= playerRange && harvestable)
+            {
+                if (Input.GetMouseButton(0))
+                {
+                    harvestCrop();
+                }
+                rend.color = inRangeColor;
+            }
+            else
+                rend.color = outOfRangeColor;
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        rend.color = originalColor;
+    }
+
 }
--- Assets/Scripts/DayNightScript.cs
+++ Assets/Scripts/DayNightScript.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.WindowsRuntime;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,22 +9,36 @@ using UnityEngine.UI;
 public class DayNightScript : MonoBehaviour
 {
     [Header("Time Varibles")]
-    [Tooltip("How long you want the village loop to last (Hours)")]
-    public float TimeTarget;
+    //[Tooltip("How long you want the village loop to last (Hours)")]
+    //public float TimeTarget;
     [Tooltip("What hour the day starts at")]
     public float dayStartTime;
     public bool TimerOn = false;
     private bool isPm = false;
     private float TimeElapsed;      // 1 hour is 1 minute in real life
+    private static float hour;
+    private static float day;
 
     [Header("Links")]
     public TextMeshProUGUI TimerTxt;
+    public static DayNightScript current;
+
+    [Header("DEBUG VARIBLES")]
+    public float timeScale;
+
+
+    private void Awake()
+    {
+        current = this;
+    }
 
     void Start()
     {
+        //TimeTarget *= 60;
         TimerOn = true;
-        TimeTarget *= 60;
-        TimeElapsed = 0;
+        TimeElapsed = dayStartTime * 60;
+        day = 1;
+        Time.timeScale = timeScale;
     }
 
     void Update()
@@ -30,17 +46,8 @@ public class DayNightScript : MonoBehaviour
         // Checks if the TimeElapsed has passed the TimeTarget and stops the timer if it has
         if (TimerOn)
         {
-            if (TimeElapsed < TimeTarget)
-            {
-                TimeElapsed += Time.deltaTime;
-                updateTimer(TimeElapsed);
-            }
-            else
-            {
-                Debug.Log("Time is UP!");
-                TimeElapsed = 0;
-                TimerOn = false;
-            }
+            TimeElapsed += Time.deltaTime;
+            updateTimer(TimeElapsed);
         }
     }
 
@@ -49,17 +56,56 @@ public class DayNightScript : MonoBehaviour
     {
         currentTime += 1;
 
-        float minutes = Mathf.FloorToInt(currentTime / 60);
+        hour = Mathf.FloorToInt(currentTime / 60);
 
-        minutes += dayStartTime;
+        if (hour > 23)
+        {
+            hour = hour % 24;                                   // Resets clock if it is past midnight and changes day
+            TimeElapsed = 0;
+            day++;
+            isPm = false;                                       // Changes time to AM
+            newDay();                                           // Calls New Day event to let all scripts that are subscribed know it is a new day
+        }
 
-        if (minutes > 11)
+        if (hour > 11 && !isPm)                                 // Changes time to PM if needed
             isPm = true;
 
-        if(!isPm)
-            TimerTxt.text = string.Format("{0} AM", minutes);
+        if (!isPm)
+        {
+            if (hour == 0)
+                TimerTxt.text = "12 AM";
+            else
+                TimerTxt.text = string.Format("{0} AM", hour);
+        }
         else
-            TimerTxt.text = string.Format("{0} PM", minutes - 12);
+        {
+            if (hour == 12)
+                TimerTxt.text = "12 PM";
+            else
+                TimerTxt.text = string.Format("{0} PM", hour - 12);
+        }   
     }
+
+    // Returns the hour it currently is with 0 being midnight and 12 being noon (Ex: 3pm returns 15)
+    public static float getHour()
+    {
+        return hour;
+    }
+
+    // Returns what day the game is on (first day is day 1)
+    public static float getDay()
+    {
+        return day;
+    }
+
+    public event Action onNewDay;
+    public void newDay()
+    {
+        if(onNewDay != null)
+        {
+            onNewDay();
+        }
+    }
+    
 
 }
