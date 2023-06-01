014a589899b79869839bba577eb8b776b1148a8d
Nathan Gardea <natecgardea@gmail.com>
2023-06-01 15:51:32 -0700

    Inventory Backend

---
+++ Assets/Scripts/Inventory Scripts/InventorySlot.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class InventorySlot
+{
+    [SerializeField] private InventoryItemData itemData;
+    [SerializeField] private int stackSize;
+
+    public InventoryItemData Data => itemData;
+    public int StackSize => stackSize;
+
+    public InventorySlot(InventoryItemData source, int amount)
+    {
+        itemData = source;
+        stackSize = amount;
+    }
+    public InventorySlot()
+    {
+        itemData = null;
+        stackSize = -1;
+    }
+
+    public void ClearSlot()
+    {
+        itemData = null;
+        stackSize = -1;
+    }
+    public bool RoomLeftInStack(int amountToAdd, out int amountRemaining)
+    {
+        amountRemaining = itemData.MaxStackSize - stackSize;
+
+        return RoomLeftInStack(amountToAdd);
+    }
+
+    public bool RoomLeftInStack(int amountToAdd)
+    {
+        if (stackSize + amountToAdd <= itemData.MaxStackSize) return true;
+        else return false;
+    }
+
+    public void AddToStack(int amount)
+    {
+        stackSize += amount;
+    }
+
+    public void RemoveFromStack(int amount)
+    {
+        stackSize -= amount;
+    }
+    
+}
