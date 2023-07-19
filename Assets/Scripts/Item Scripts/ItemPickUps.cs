925d459bbc6c1119cfeece3592955ee2014ed17b
Plutzz <benjiongco@gmail.com>
2023-07-18 20:06:49 -0700

    Item Magnet and anim

--- Assets/Scripts/Item Scripts/ItemPickUps.cs
+++ Assets/Scripts/Item Scripts/ItemPickUps.cs
@@ -1,20 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 [RequireComponent(typeof(CircleCollider2D))]
 public class ItemPickUps : MonoBehaviour
 {
-    public float PickUpRadius = 1f;
+    public float PickUpRadius = 0.5f;
     public InventoryItemData ItemData;
+    public float magnetStrength = 2;
 
     private CircleCollider2D myCollider;
+
+    private bool hasTarget;
+    private Vector3 targetPosition;
+    private Rigidbody2D rb;
 
     private void Awake()
     {
         myCollider = GetComponent<CircleCollider2D>();
         myCollider.isTrigger = true;
         myCollider.radius = PickUpRadius;
+        rb = GetComponent<Rigidbody2D>();
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -25,7 +32,22 @@ public class ItemPickUps : MonoBehaviour
 
         if (inventory.InventorySystem.AddToInventory(ItemData, 1))
         {
+
             Destroy(this.gameObject);
         }
+    }
+
+    private void FixedUpdate()
+    {
+        if (hasTarget)
+        {
+            Vector2 targetDirection = (targetPosition - transform.position).normalized;
+            rb.velocity = new Vector2(targetDirection.x, targetDirection.y) * magnetStrength;
+        }
+    }
+    public void SetTarget(Vector3 position)
+    {
+        targetPosition = position;
+        hasTarget = true;
     }
 }
---
+++ Assets/Scripts/Item Scripts/Magnet.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class Magnet : MonoBehaviour
+{
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.TryGetComponent<ItemPickUps>(out ItemPickUps item))
+        {
+            item.SetTarget(transform.parent.position);
+        }
+    }
+}
